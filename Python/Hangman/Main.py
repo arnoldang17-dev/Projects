7de73bd47ea4b1d350cb3494b08fd27bcf2498c2
Arnold <arnoldang17@gmail.com>
2023-02-17 20:04:31 +0800

    Stage 4

--- Python/Hangman/Main.py
+++ Python/Hangman/Main.py
@@ -4,8 +4,9 @@ print("H A N G M A N")
 code_list = ('python', 'java', 'swift', 'javascript')
 
 code = random.choice(code_list)
-
-answer = input("Guess the word: ")
+sub_code = code[:3] + code[3:].replace(code[3:], "-" * len(code[3:]))
+print(code)      
+answer = input("Guess the word: " + sub_code + ":  ")
 
 if code == answer:
     print("You survived!")
