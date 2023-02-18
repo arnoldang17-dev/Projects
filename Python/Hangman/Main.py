83db16d863d5d456084f3bf903084e4e5f395ab0
Arnold <arnoldang17@gmail.com>
2023-02-18 11:46:17 +0800

    Stage 5

--- Python/Hangman/Main.py
+++ Python/Hangman/Main.py
@@ -1,14 +1,27 @@
 import random
 
-print("H A N G M A N")
+print("H A N G M A N\n")
 code_list = ('python', 'java', 'swift', 'javascript')
 
 code = random.choice(code_list)
-sub_code = code[:3] + code[3:].replace(code[3:], "-" * len(code[3:]))
-print(code)      
-answer = input("Guess the word: " + sub_code + ":  ")
+# sub_code = code[:3] + code[3:].replace(code[3:], "-" * len(code[3:]))
+attempts = 8
+code2 = list("-" * len(code))
 
-if code == answer:
-    print("You survived!")
-else:
-    print("You lost!")
+while attempts:
+    print("".join(code2))    
+    answer = input("Input a letter: ")
+    
+    if code.__contains__(answer):
+        index = code.find(answer)
+        while index != -1:
+            code2[index] = answer
+            index = code.find(answer, index + 1)
+    else:
+        print(f"That letter doesn't appear in the word.")
+    
+    attempts -= 1
+
+    if "".join(code2) == code or attempts == 0:
+        print("\nThanks for playing!")
+        break
