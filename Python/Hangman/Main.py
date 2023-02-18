e4b6a2be4b7e6f22985611b405f92aab19ad9f3f
Arnold <arnoldang17@gmail.com>
2023-02-18 13:19:30 +0800

    Stage 7

--- Python/Hangman/Main.py
+++ Python/Hangman/Main.py
@@ -13,29 +13,35 @@ while attempts:
     try:
         print("".join(code2))    
         answer = input("Input a letter: ")
-        if len(answer) > 1: raise Exception
-        
+        if len(answer) > 1 or answer == "": 
+            raise Exception
+        elif answer.isupper() or not answer.isalpha():
+            print("Please, enter a lowercase letter from the English alphabet.")
+            continue
+            
         if code.__contains__(answer) and answer not in all_answer:
             all_answer.append(answer)
             index = code.find(answer)
             while index != -1:
                 code2[index] = answer
                 index = code.find(answer, index + 1)
+                
         elif answer in all_answer:
-            print("No improvements.")
-            attempts -= 1
+            print("You've already guessed this letter.")
             
         else:
             attempts -= 1
+            all_answer.append(answer)
             print("That letter doesn't appear in the word.")
                  
     except Exception:
+        print("Please, input a single letter.")
         continue;
         
     if "".join(code2) == code:
-        print("".join(code2))
-        print("You guessed the word!")
+        print(f"You guessed the word {code}!")
         print("You survived!")
         break
+    
     elif attempts == 0:
-        print("You lost!")
+        print("\nYou lost!")
